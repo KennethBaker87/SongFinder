11241f2fb159f3717b3c4c822f211da3d31506de
Kenneth Baker <spyksaturn@gmail.com>
2023-04-04 09:43:52 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SongFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
